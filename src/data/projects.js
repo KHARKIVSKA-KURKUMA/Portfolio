f0b139e6a201dc4ae17926e9fcfce3a6d21d3b55
KHARKIVSKA-KURKUMA <catherine.vasyuk11@gmail.com>
2024-01-22 01:23:45 +0200

    added projects to my portfolio

--- src/data/projects.js
+++ src/data/projects.js
@@ -18,15 +18,6 @@ export const projects = [
       "Individual project. This site is designed to help users easily rent cars for their transportation needs. Whether you're planning a road trip, need a vehicle for daily commuting, or just want to try out a different car, we've got you covered.",
   },
   {
-    overlay: "/src/img/Overlays/ice-cream.png",
-    title: "Ice Cream",
-    tools: ["SCSS", "HTML", "JS", "Parcel"],
-    href: "https://github.com/KHARKIVSKA-KURKUMA/IceCream",
-    website: "https://kharkivska-kurkuma.github.io/IceCream/",
-    description:
-      "Team project. Played a key role in developing the contact section, slider gallery and menu for the ice-cream distribution company website, contributing to a visually appealing and user-friendly platform. ",
-  },
-  {
     overlay: "/src/img/Overlays/healthy-studio.png",
     title: "Healthy Studio",
     tools: ["SCSS", "HTML", "JS", "Parcel"],
@@ -34,6 +25,15 @@ export const projects = [
     website: "https://kharkivska-kurkuma.github.io/Healthy-Studio/",
     description:
       "Individual project. The main content of the website consists of information about a health studio, including details about their services, team, and approach to health and wellness.",
+  },
+  {
+    overlay: "/src/img/Overlays/ice-cream.png",
+    title: "Ice Cream",
+    tools: ["SCSS", "HTML", "JS", "Parcel"],
+    href: "https://github.com/KHARKIVSKA-KURKUMA/IceCream",
+    website: "https://kharkivska-kurkuma.github.io/IceCream/",
+    description:
+      "Team project. Played a key role in developing the contact section, slider gallery and menu for the ice-cream distribution company website, contributing to a visually appealing and user-friendly platform. ",
   },
   {
     overlay: "/src/img/Overlays/hair-factory.png",
@@ -47,10 +47,37 @@ export const projects = [
   {
     overlay: "/src/img/Overlays/goose-track.png",
     title: "Goose Track",
-    tools: ["SCSS", "HTML", "JS", "Parcel"],
+    tools: ["React", "Redux", "JS", "Material UI"],
     href: "https://github.com/KHARKIVSKA-KURKUMA/GooseTrack",
     website: "https://kharkivska-kurkuma.github.io/GooseTrack/",
     description:
-      "Team project. Played a key role in developing the contact section, slider gallery and menu for the ice-cream distribution company website, contributing to a visually appealing and user-friendly platform. ",
+      "Team project. As the Frontend Team Lead, I spearheaded the development of our streamlined productivity and task management platform. I crafted the project skeleton, developed a calendar page for managing schedules, and created a comprehensive statistics page for visualizing productivity data.",
+  },
+  {
+    overlay: "/src/img/Overlays/web-studio.png",
+    title: "Web Studio",
+    tools: ["SCSS", "HTML", "JS", "Parcel"],
+    href: "https://github.com/KHARKIVSKA-KURKUMA/Web-Studio",
+    website: "https://kharkivska-kurkuma.github.io/Web-Studio/",
+    description:
+      "Individual project. The website features a modern and minimalist design with a white background and bold typography. The navigation menu is located at the header and links to different sections on the site, including the homepage, portfolio, services, and about page.",
+  },
+  {
+    overlay: "/src/img/Overlays/vantage-studio.png",
+    title: "Vantage Studio",
+    tools: ["SCSS", "HTML", "JS", "Parcel"],
+    href: "https://github.com/KHARKIVSKA-KURKUMA/Vantage-Studio",
+    website: "https://kharkivska-kurkuma.github.io/Vantage-Studio/",
+    description:
+      "Individual project. The main content of the website consists of a portfolio of the studio's web design projects, along with information about their services and team.",
+  },
+  {
+    overlay: "/src/img/Overlays/currency-converter.png",
+    title: "Currency Converter",
+    tools: ["React", "Redux", "JS", "Material UI"],
+    href: "https://github.com/KHARKIVSKA-KURKUMA/Currency-Converter",
+    website: "https://kharkivska-kurkuma.github.io/Currency-Converter/",
+    description:
+      "Individual project. This web application allows users to convert currencies easily and quickly.",
   },
 ];
